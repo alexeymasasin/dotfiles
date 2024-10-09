3181fb952a969ff0ff5b2ef7bf17fcf5ac769831
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-10 04:42:01 +0500

    Update

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -9,9 +9,3 @@ if [[ $(xrandr -q | grep 'DisplayPort-2 connected') ]]; then
     polybar external &
 fi
 
-xrandr --addmode DisplayPort-2 1366x768 &
-xrandr --output DisplayPort-2 --mode 1366x768 --left-of HDMI-A-0 &
-
-if [[ $(xrandr -q | grep 'DisplayPort-2 disconnected 1366x768+0+0') ]] then
-	polybar external &
-fi
