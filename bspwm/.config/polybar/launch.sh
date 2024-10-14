ce72ec7e24ae23c08e7b5dc6a76cb6805d1ca631
Alexey Masasin <alexeymasasin@gmail.com>
2024-10-14 09:14:29 +0500

    Update

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -5,7 +5,8 @@ killall -q polybar
 
 polybar example &
 
-if [[ $(xrandr -q | grep 'DisplayPort-2 connected') ]]; then
+if [[ $(xrandr -q | grep 'DisplayPort-0 connected') ]]; then
     polybar external &
+    xrandr --output DisplayPort-0 --mode 1920x1080 --below HDMI-A-0 &
 fi
 
