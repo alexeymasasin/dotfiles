544b73deab111d9ae5166a7eeaf3c808a7a72ce6
Alexey Masasin <alexeymasasin@gmail.com>
2024-11-01 07:25:01 +0500

    Updates & fixes

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -7,7 +7,7 @@ polybar main &
 
 if [[ $(xrandr -q | grep 'DisplayPort-0 connected') ]]; then
     polybar main-external &
-    xrandr --addmode DisplayPort-0 1920x1080 &
-    xrandr --output DisplayPort-0 --mode 1920x1080 --left-of HDMI-A-0 &
+    xrandr --addmode DisplayPort-0 1366x768 &
+    xrandr --output DisplayPort-0 --mode 1366x768 --left-of HDMI-A-0 &
 fi
 
