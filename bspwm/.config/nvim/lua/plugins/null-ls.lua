eda38703b6826b95ef3eca4d627cb677b9972696
Alexey Masasin <alexeymasasin@gmail.com>
2024-11-04 12:25:23 +0500

    Neovim config update

--- bspwm/.config/nvim/lua/plugins/null-ls.lua
+++ bspwm/.config/nvim/lua/plugins/null-ls.lua
@@ -1,23 +1,57 @@
 return {
-    "stevearc/conform.nvim",
-    dependencies = {
-    },
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = {},
     config = function()
-        require("conform").setup({
-  formatters_by_ft = {
-    javascript = { { 'prettierd', "prettier" } },
-    javascriptreact = { { 'prettierd', "prettier" } },
-    typescript = { { 'prettierd', "prettier" } },
-    typescriptreact = { { 'prettierd', "prettier" } },
-    javascriptreact = { { 'prettierd', "prettier" } },
-    json = { { 'prettierd', "prettier" } },
-    html = { { 'prettierd', "prettier" } },
-    css = { { 'prettierd', "prettier" } },
-  },
-    format_on_save = {
-        lsp_fallback = true,
-        timeout_ms = 500
-    },
-})
-    end
+        local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.eslint_d.with({
+                    filetypes = {
+                        "typescript",
+                        "javascript",
+                        "typescriptreact",
+                        "javascriptreact",
+                    },
+                }),
+                null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.diagnostics.ltrs,
+                null_ls.builtins.formatting.prettierd.with({
+                    filetypes = {
+                        "css",
+                        "scss",
+                        "less",
+                        "html",
+                        "json",
+                        "jsonc",
+                        "yaml",
+                        "markdown",
+                        "markdown.mdx",
+                        "graphql",
+                        "handlebars",
+                    },
+                }),
+            },
+            on_attach = function(client, bufnr)
+                -- Enable formatting on sync
+                if client.supports_method("textDocument/formatting") then
+                    local format_on_save = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = format_on_save,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({
+                                bufnr = bufnr,
+                                filter = function(_client)
+                                    return client.name == "null-ls"
+                                end,
+                            })
+                        end,
+                    })
+                end
+            end,
+        })
+    end,
 }
